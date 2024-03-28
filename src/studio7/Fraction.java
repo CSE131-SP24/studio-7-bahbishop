7b74115ad16ff3d3f296f0d27345153a6ec71376
ibrahim <ibrahim@localhost>
2024-03-28 11:17:51 -0500

    Studio 7 is a wrap!!

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -1,5 +1,22 @@
 package studio7;
 
 public class Fraction {
+private int numerator;
+private int denominator;
+
+public Fraction(int initNumerator, int initDenominator) {
+	numerator = initNumerator;
+	denominator = initDenominator;
+}
+public Fraction addedTo(Fraction frac) {
+	int commonDenom = this.denominator * frac.denominator;
+	int commonNum = (this.numerator * frac.denominator) + (frac.numerator * this.denominator);
+	Fraction sum = new Fraction(commonNum,commonDenom);
+	return sum;
+}
+public Fraction productTo(Fraction frac) {
+	return new Fraction((this.numerator * frac.numerator),(this.denominator * frac.denominator));
+	
+}
 
 }
